79cb6ec80db53efbe598d586cf3465233bb3a3e7
sarthak2131 <sarthakbaderiya19@gmail.com>
2025-01-23 14:10:25 +0530

    update MongoDB connection to use environment variables and add start script

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,16 @@
-const mongoose = require('mongoose');
+import dotenv from 'dotenv';
+dotenv.config(); // Loads environment variables from .env file
+
+import mongoose from 'mongoose';
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DB_URL, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log('MongoDB connected');
-    } catch (error) {
-        console.error('Error connecting to MongoDB:', error);
-        process.exit(1);
-    }
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {});
+    console.log("Successfully Connected");
+  } catch (err) {
+    console.log("Error occurred: ", err);
+    process.exit(1);
+  }
 };
 
-module.exports = connectDB;
+export default connectDB;
